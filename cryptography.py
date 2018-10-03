70b37a3f28adbf5f182fdb60478ebf070a981993
NIckLee-hanover <42844776+NIckLee-hanover@users.noreply.github.com>
2018-10-03 13:12:31 -0400

    Updated from Brython Server: 10/3/2018 1:12:30 PM

--- cryptography.py
+++ cryptography.py
@@ -1,12 +1,67 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Nick Lee
+Credit: none yet
 
-Assignment:
+Assignment: Cryptography
 
 Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!☃"
+message = 'hello world'
+key = 'hi'
+output = ''
+keyp = 0
+messagep = -1
+letter = 0
+def encrypt(m,k):
+    global chars, keyp, messagep, letter
+    for i in range(len(m)):
+        messagep = 0
+        keyp = 0
+        if letter >= len(k):
+            letter = 0
+        else: 
+            letter += 1
+        while m[i] != chars[messagep] or i == len(m):
+            messagep += 1
+        print(messagep)
+        for j in range(len(chars)):
+            if k[letter] == chars[j]:
+                break
+            keyp += 1
+        print(keyp,'q')
+        
+encrypt('aabbajsba;jbfasdfa', 'hello worldz')
+
+
+
+
+"""
+
+def enter():
+    global message, key
+    start = input('Enter e to encrypt, d to decrypt, or q to quit:') 
+    if start == 'e' or 'd' or 'q':
+        if start == 'e':
+            message = input("Message: ")
+            key = input("Key: ")
+        elif start == 'd':
+            message = input("Message: ")
+            key = input("Key: ")
+        elif start == 'q':
+            print('Goodbye!')
+            #break
+            
+        else:
+            print("Did not understand command, try again.")
+            enter()
+
+enter()
+"""
+
+
+
+print("Hello, world.")
